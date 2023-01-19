88e7fb3096275f86e7ffdc5c36ffa59b6c2ccdf2
Nicholas Galante <nicholasgalante94@gmail.com>
2023-01-19 17:50:46 -0500

    commit tree e65071019797226d97cd75466ecbd06b0ec1c08c

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+      .then(res => res.json())
+      .then(plants => setPlants(plants))
+  }, [plants])
+
+  const plantsDisplayed = plants.map(plant => {
+    return <PlantCard key={plant.id} plant={plant} />
+  })
+
+  fetch("http://localhost:6001/plants")
+
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">{plantsDisplayed}</ul>
   );
 }
 
